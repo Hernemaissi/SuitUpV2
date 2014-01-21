edbad71e498f1e40f175e9a479653bfe2df5dfa1
Toni Maki <maki.toni@gmail.com>
2014-01-21 13:04:01 +0200

    Initial commit with empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Suitupv2::Application.config.secret_key_base = 'b68de8a786e79343a4c860985da13b723a1e0cc89fc0ea783a7fddbee2319bedcf437bca6845ed247898db2fca7e3d1015fa7d2882395e3ff4cd81ba8f18c3cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Suitupv2::Application.config.session_store :cookie_store, key: '_suitupv2_session'
